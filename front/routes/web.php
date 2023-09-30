434c5da9762b0dd3702904ae8c06707edfddc5d0
Fred Orozco Diaz <oroskodias@gmail.com>
2023-09-30 18:15:52 -0400

    Update routes

--- front/routes/web.php
+++ front/routes/web.php
@@ -22,6 +22,9 @@ Auth::routes(['register' => false]);
 // Internal routes
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/products/create', [App\Http\Controllers\Products::class, 'createProductPage'])->name('createProductPage');
+    Route::get('/category/{categoryTitle}', [App\Http\Controllers\HomeController::class, 'getProductsByCategory'])->name('getProductsByCategory');
+    Route::get('/branches', [App\Http\Controllers\HomeController::class, 'showBranchesPage'])->name('showBranchesPage');
 });
 
 // External routes
@@ -46,3 +49,8 @@ Route::prefix('/api/v1/')->group(function () {
     Route::get('/products/branch/{id}', [\App\Models\RestApi\Products::class, 'getProducts'])
         ->name('get-products');
 });
+
+// Views
+Route::get('/internal/getProductsView', [\App\Models\RestApi\Products::class, 'getProductsView'])
+    ->name('get-products-view');
+
