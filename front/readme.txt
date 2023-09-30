76298e13af434aca980e5385a5648e03d1c25769
Fred Orozco Diaz <oroskodias@gmail.com>
2023-09-30 16:22:29 -0400

    Add rest of front files

---
+++ front/readme.txt
@@ -0,0 +1,2 @@
+// php artisan tinker
+// DB::table('users')->insert(['name'=>'Fred','email'=>'oroskodias@gmail.com','password'=>Hash::make('1234')])
--- front/routes/web.php
+++ front/routes/web.php
@@ -14,5 +14,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('home');
+});
+
+Auth::routes(['register' => false]);
+
+// Internal routes
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
+// External routes
+Route::prefix('/api/v1/')->group(function () {
+    // Get all branches
+    Route::get('/branches', [\App\Models\RestApi\Branches::class, 'getBranches'])
+        ->name('get-branches');
+
+    // Get all conditions
+    Route::get('/conditions', [\App\Models\RestApi\Conditions::class, 'getConditions'])
+        ->name('get-conditions');
+
+    // Get all categories
+    Route::get('/categories', [\App\Models\RestApi\Categories::class, 'getCategories'])
+        ->name('get-categories');
 });
