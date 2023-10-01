7ef44b103b33cdada8ea4f4286b4e64d3856f7a5
Fred Orozco Diaz <oroskodias@gmail.com>
2023-09-30 20:38:37 -0400

    Update branches code

--- front/app/Http/Controllers/Branches.php
+++ front/app/Http/Controllers/Branches.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 class Branches extends Controller
 {
-    public function index()
+    public function showBranchesPage()
     {
-        
+        $branchesModel = new \App\Models\RestApi\Branches();
+        $branchesList = $branchesModel->getBranches(true);
+        return view('branches', compact('branchesList'));
     }
 
 }
